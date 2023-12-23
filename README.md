3a96b294c73d7f811a3b250779747dca2c0413af
unknown <thomas.dumond2001@gmail.com>
2023-12-23 22:10:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Spartacus Project
+<img src="https://img.shields.io/badge/Version-0.0.1-blue.svg" alt="Version">
+<img src="https://img.shields.io/badge/License-AGPL--3.0-blue.svg" alt="License">
+<img src="https://img.shields.io/badge/Status-Active-green.svg" alt="Status">
+
+## Table of Contents
+1. [Table of Contents](#table-of-contents)
+2. [Introduction](#introduction)
+3. [Dependencies](#dependencies)
+4. [Installation](#installation)
+5. [Usage](#usage)
+6. [Contributing](#contributing)
+7. [License](#license)
+
+## Introduction
+This is a top secret project.  It is so secret that we can't even tell you what it is.  But it is awesome.
+
+## Dependencies
+- [Flutter](https://flutter.dev/docs/get-started/install)
+
+## Installation
+1. Clone the repository
+2. Make sure you have Flutter installed
+3. Run `flutter pub get` to install dependencies
+4. Run `flutter run` to run the app
+
+## Usage
+1. Open the app
+2. Use the app
+3. Enjoy the app
+
+## Contributing
+For the moment, we are not accepting contributions. Maybe in the future.
+
+## License
+[AGPL-3.0](https://www.gnu.org/licenses/agpl-3.0.en.html)
---
+++ lib/main.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'dart:math';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spartacus Project',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.pink,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Spartacus Project Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _currentIndex = 0;
+  final player = AudioPlayer();
+  bool isPlaying = false;
+  double currentPosition = 0.0;
+  double bufferedPosition = 0.0;
+  double maxDuration = 1.0;
+  String currentSong = '';
+  List<String> songs = [
+    'Resonance',
+    'Line',
+    'Interlude',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    currentSong = songs[0];
+    initAudio();
+  }
+
+  Future<void> initAudio() async {
+    String currentUrl =
+        "http://localhost/audio/erratic/${currentSong.toLowerCase()}.wav";
+    var file;
+    try {
+      file = await DefaultCacheManager().getSingleFile(currentUrl);
+    } catch (e) {
+      print('Le fichier $currentSong n\'a pas pu être téléchargé');
+    }
+    if (file != null) {
+      await player.setFilePath(file.path);
+      player.positionStream.listen((duration) {
+        setState(() {
+          currentPosition = duration.inMilliseconds.toDouble();
+          maxDuration = player.duration?.inMilliseconds.toDouble() ?? 0.0;
+        });
+      });
+      player.bufferedPositionStream.listen((duration) {
+        setState(() {
+          bufferedPosition =
+              min(duration.inMilliseconds.toDouble(), maxDuration);
+        });
+      });
+      maxDuration = player.duration?.inMilliseconds.toDouble() ?? 0.0;
+    } else {
+      print('Le fichier $currentSong n\'a pas pu être téléchargé');
+    }
+  }
+
+  @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
+  }
+
+  Future<void> _playAudio() async {
+    await player.play();
+    setState(() {
+      isPlaying = true;
+    });
+  }
+
+  Future<void> _pauseAudio() async {
+    await player.pause();
+    setState(() {
+      isPlaying = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      bottomNavigationBar: SalomonBottomBar(
+        currentIndex: _currentIndex,
+        onTap: (i) => setState(() => _currentIndex = i),
+        items: [
+          /// Home
+          SalomonBottomBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Home"),
+            selectedColor: Colors.purpleAccent,
+          ),
+
+          /// Search
+          SalomonBottomBarItem(
+            icon: Icon(Icons.search),
+            title: Text("Search"),
+            selectedColor: Colors.orangeAccent,
+          ),
+
+          /// Profile
+          SalomonBottomBarItem(
+            icon: Icon(Icons.person),
+            title: Text("Artist"),
+            selectedColor: Colors.tealAccent,
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            DropdownButton<String>(
+              value: currentSong,
+              onChanged: (String? newValue) {
+                if (newValue != null && newValue != currentSong) {
+                  setState(() {
+                    isPlaying = false;
+                    currentSong = newValue;
+                  });
+                  initAudio();
+                }
+              },
+              items: songs.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            Stack(
+              children: <Widget>[
+                ProgressBar(
+                  progress: Duration(milliseconds: currentPosition.toInt()),
+                  buffered: Duration(milliseconds: bufferedPosition.toInt()),
+                  total: Duration(milliseconds: maxDuration.toInt()),
+                  onSeek: (duration) {
+                    player.seek(duration);
+                    setState(() {
+                      currentPosition = duration.inMilliseconds.toDouble();
+                    });
+                  },
+                ),
+              ],
+            ),
+            IconButton(
+              icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+              onPressed: isPlaying ? _pauseAudio : _playAudio,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
