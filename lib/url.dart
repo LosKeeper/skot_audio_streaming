62f176996d6312e76d72878cc081e4717474fc91
unknown <thomas.dumond2001@gmail.com>
2024-03-04 18:04:46 +0100

    fix when no song selected

--- lib/url.dart
+++ lib/url.dart
@@ -1,7 +1,7 @@
-bool debug = false;
+import 'package:flutter/foundation.dart';
 
 String url = 'https://loskeeper.fr:42024';
-String liveUrl = debug
+String liveUrl = kDebugMode
     ? 'https://loskeeper.fr:42002/debug'
     : 'https://loskeeper.fr:42002/stream';
 String liveUrlInfo = 'https://loskeeper.fr:42002/status.xsl';
@@ -9,4 +9,5 @@ String liveUrlInfo = 'https://loskeeper.fr:42002/status.xsl';
 String availableSongsUrl = '$url/audio/available_songs.json';
 String availableAlbumsUrl = '$url/audio/available_albums.json';
 
-String mountPointName = debug ? 'Mount Point /debug' : 'Mount Point /stream';
+String mountPointName =
+    kDebugMode ? 'Mount Point /debug' : 'Mount Point /stream';
